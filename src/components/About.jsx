a56b22d1e681c276bf8e5935b33a1cbf63697817
MichalMiemus <158090652+MichalMiemus@users.noreply.github.com>
2024-06-07 18:58:49 +0100

    better employee portraits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,21 +8,21 @@ const employees = [
     position: "dział 1",
     description:
       "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e1.jpg`,
+    photo: `./img/e1.webp`,
   },
   {
     name: "Imię Nazwisko 2",
     position: "dział 2",
     description:
       "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e2.jpg`,
+    photo: `./img/e2.webp`,
   },
   {
     name: "Imię Nazwisko 3",
     position: "dział 3",
     description:
       "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e1.jpg`,
+    photo: `./img/e3.webp`,
   },
 ];
 
