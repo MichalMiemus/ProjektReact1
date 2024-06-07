2528064ac5b46fda65cf10ff112ebcbcfdcf93dd
MichalMiemus <158090652+MichalMiemus@users.noreply.github.com>
2024-06-07 16:25:12 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,6 +17,13 @@ const employees = [
       "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
     photo: `./img/e2.jpg`,
   },
+  {
+    name: "Imię Nazwisko 3",
+    position: "dział 3",
+    description:
+      "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
+    photo: `./img/e1.jpg`,
+  },
 ];
 
 const About = () => {
