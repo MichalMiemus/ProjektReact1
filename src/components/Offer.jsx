cb557fc6d5b2fe0a25eb0a22d21c3d8955cb2fad
MichalMiemus <158090652+MichalMiemus@users.noreply.github.com>
2024-06-07 16:24:31 +0100

    Update Offer.jsx

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -7,7 +7,7 @@ const offers = [
   { title: "Usługa 2", isNew: false },
   { title: "Usługa 3", isNew: false },
   { title: "Usługa 4", isNew: false },
-  { title: "Usługa 5", isNew: true },
+  { title: "Usługa 5", isNew: false },
   { title: "Usługa 6", isNew: false },
 ];
 
