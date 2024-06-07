e75b8735d17d3b67df995f92b356cf45932184e8
MichalMiemus <158090652+MichalMiemus@users.noreply.github.com>
2024-06-07 22:43:27 +0100

    portraits resize and change type

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,28 +8,28 @@ const employees = [
     position: "dział 1",
     description:
       "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e1.webp`,
+    photo: `./img/e1.jpg`,
   },
   {
     name: "Imię Nazwisko 2",
     position: "dział 2",
     description:
       "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e2.webp`,
+    photo: `./img/e2.jpg`,
   },
   {
     name: "Imię Nazwisko 3",
     position: "dział 3",
     description:
       "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e3.webp`,
+    photo: `./img/e3.jpg`,
   },
   {
     name: "Imię Nazwisko 4",
     position: "dział 4",
     description:
       "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
-    photo: `./img/e4.webp`,
+    photo: `./img/e4.jpg`,
   },
 ];
 
